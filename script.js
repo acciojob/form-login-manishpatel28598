85a05c61df890aaf42edb8324c96b88db983dbcd
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-03-10 11:40:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	const fname = document.getElementById('fname').value;
+	const lname = document.getElementById('lname').value;
+	
+	alert(`${fname} ${ lname}`);
+	
 
 }
